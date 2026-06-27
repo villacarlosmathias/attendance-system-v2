2b9df8463d93274fe66499021e0964e7e787c9bf
Mathias Villacarlos <mattyvillacarlos@Mathiass-MacBook-Air.local>
2026-06-27 09:06:32 +0800

    Restore backend server

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,15 @@
-require("dotenv").config();
-
 const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected PostgreSQL error:", err);
 });
 
 module.exports = pool;
